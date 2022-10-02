77eb6e4b6e5d359f1e1e995e9b762858826c3def
Geoff Ford <gford1000@hotmail.com>
2022-10-02 19:50:37 +0100

    Added code to provide signing key from environment variables

--- lambda/createkeys/go.mod
+++ lambda/createkeys/go.mod
@@ -4,6 +4,22 @@ go 1.18
 
 require (
 	github.com/aws/aws-lambda-go v1.34.1
+	github.com/aws/aws-sdk-go-v2/config v1.17.8
+	github.com/aws/aws-sdk-go-v2/service/kms v1.18.11
 	github.com/gford1000-go/pkigen v0.0.0-20221001173721-fde2e23cc7a2
 	github.com/gford1000-serverless/util v0.0.0-20221002175534-fefa4f9c53aa
+)
+
+require (
+	github.com/aws/aws-sdk-go-v2 v1.16.16 // indirect
+	github.com/aws/aws-sdk-go-v2/credentials v1.12.21 // indirect
+	github.com/aws/aws-sdk-go-v2/feature/ec2/imds v1.12.17 // indirect
+	github.com/aws/aws-sdk-go-v2/internal/configsources v1.1.23 // indirect
+	github.com/aws/aws-sdk-go-v2/internal/endpoints/v2 v2.4.17 // indirect
+	github.com/aws/aws-sdk-go-v2/internal/ini v1.3.24 // indirect
+	github.com/aws/aws-sdk-go-v2/service/internal/presigned-url v1.9.17 // indirect
+	github.com/aws/aws-sdk-go-v2/service/sso v1.11.23 // indirect
+	github.com/aws/aws-sdk-go-v2/service/ssooidc v1.13.6 // indirect
+	github.com/aws/aws-sdk-go-v2/service/sts v1.16.19 // indirect
+	github.com/aws/smithy-go v1.13.3 // indirect
 )
--- lambda/createkeys/main.go
+++ lambda/createkeys/main.go
@@ -3,12 +3,18 @@ package main
 import (
 	"context"
 	"createkeys/pkg/handlers"
+	"encoding/base64"
+	"encoding/json"
 	"errors"
+	"fmt"
 	"os"
 	"strconv"
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/service/kms"
+	"github.com/gford1000-go/pkigen"
 	util "github.com/gford1000-serverless/util/events"
 )
 
@@ -28,9 +34,56 @@ func init() {
 		size = i
 	}
 
-	// Load and set signing key here, to sign responses
+	// Default to no signing
 	responder = util.NewGatewayProxyResponder(nil)
 	responder.AddHeader("Content-Type", "application/json")
+
+	// See whether a signing key has been provided
+	s = os.Getenv("SIGNING_KEY")
+	keyID := os.Getenv("SIGNING_KMS_ID")
+	if len(s) > 0 && len(keyID) > 0 {
+		// This is a KMS encrypted, JSON serialised Base64EncodedRSAKey
+		// that contains a private_key attribute
+		sb, err := base64.URLEncoding.DecodeString(s)
+		if err != nil {
+			fmt.Printf("Unable to decode SIGNING_KEY: %s\n", err)
+			return
+		}
+
+		cfg, err := config.LoadDefaultConfig(context.TODO())
+		if err != nil {
+			fmt.Printf("Unable to load default context: %s\n", err)
+			return
+		}
+
+		client := kms.NewFromConfig(cfg)
+		input := &kms.DecryptInput{
+			CiphertextBlob: sb,
+			KeyId:          &keyID,
+		}
+
+		result, err := client.Decrypt(context.TODO(), input)
+		if err != nil {
+			fmt.Printf("Error decrypting with key (%s): %s\n", keyID, err)
+			return
+		}
+
+		var b pkigen.Base64EncodedRSAKey
+		err = json.Unmarshal(result.Plaintext, &b)
+		if err != nil {
+			fmt.Printf("Error unmarshaling plaintext JSON: %s\n", err)
+			return
+		}
+
+		signingPrivateKey, err := pkigen.UnmarshalPrivateKey(&b)
+		if err != nil {
+			fmt.Printf("Error extracting private key for siging: %s\n", err)
+			return
+		}
+
+		responder = util.NewGatewayProxyResponder(signingPrivateKey)
+		responder.AddHeader("Content-Type", "application/json")
+	}
 }
 
 var errInvalidHTTPMethod = errors.New("only GET or POST requests supported")
